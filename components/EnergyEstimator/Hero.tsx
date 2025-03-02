143696741bc99daf8a4ebf16453215cc9c31f6c6
Timmydee <dahunsitimmy@gmail.com>
2025-03-02 17:02:56 +0100

    update

--- components/EnergyEstimator/Hero.tsx
+++ components/EnergyEstimator/Hero.tsx
@@ -6,15 +6,18 @@ const Hero = () => {
   return (
     <section className="relative">
       <div className="max-w-8xl mx-auto px-4 md:py-8 py-6 text-center">
-        <div className="flex justify-center items-center bg-[#FAFAFA] rounded-lg p-2 max-w-[476px] mx-auto ">
+        <div className="flex gap-1 justify-center items-center bg-[#FAFAFA] rounded-lg p-2 max-w-[476px] mx-auto ">
           <Image src='/AI.webp' width={20} height={20} alt="Inverter" />
-          <p className="p2">Power Calculator to get your solar Inverter recommendation</p>
+          <p className="p2">Power Calculator to get your perfect solar Inverter setup</p>
         </div>
         <h2 className="h1 md:mt-0 pt-3">
           Estimate Your Energy Needs in Minutes!
         </h2>
-        <p className="mb-6 p1">
-        Select your usage preferences to get personalized recommendations for solar panels, inverters, and more.
+        <p className="mb-1 p1 text-center">
+        Find out the right solar panel and inverter for your home
+        </p>
+        <p className="mb-6 p1 max-w-5xl text-center mx-auto">
+        Simply select your appliances, and we’ll give you a personalized recommendation—fast and hassle-free!
         </p>
       </div>
       <div className="absolute lg:top-0 lg:left-20 md:top-4 md:left-12 md:block hidden">
