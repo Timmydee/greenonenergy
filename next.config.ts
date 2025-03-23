43ad18ec07453503cf5f82609d5d56ba48a388eb
timmydee <dahunsitimmy@gmail.com>
2025-03-23 21:57:32 +0100

    feat: cloudinary image route

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["example.com", "res.cloudinary.com"], // Add your actual image domain here
+  },
 };
 
 export default nextConfig;
