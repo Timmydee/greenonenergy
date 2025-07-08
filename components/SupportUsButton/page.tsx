f7dac89958299b7cf2a1733860efa6b9b184b047
timmydee <dahunsitimmy@gmail.com>
2025-07-08 15:20:38 +0100

    Feat: Support Page, Footer feat

---
+++ components/SupportUsButton/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Link from "next/link";
+
+export default function SupportUsButton() {
+  return (
+    <Link
+      href="https://paystack.shop/pay/emu3uso3m7"
+      target="_blank"
+      className="fixed bottom-6 right-6 z-50 bg-green-600 text-white px-5 py-3 rounded-full shadow-lg hover:bg-green-700 transition-all text-sm font-semibold"
+    >
+      ☕ Support Us
+    </Link>
+  );
+}
